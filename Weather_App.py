8b968cee73d67163300d352c1049fe0888b831e6
Vadim <Kozlov67b@gmail.com>
2024-12-10 13:04:32 +0300

    Создана базовая структура проекта и импортированы библиотеки

---
+++ Weather_App.py
@@ -0,0 +1,10 @@
+import requests
+from plyer import notification
+import os
+
+# Конфигурация
+CITY = "Пенза"
+API_KEY = "23496c2a58b99648af590ee8a29c5348" #  API ключ от OpenWeatherMap
+UNITS = "metric"
+LANG = "ru"
+APP_NAME = "Weather App"
